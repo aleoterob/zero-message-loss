bb2d57bbe8912a7973a9743f968cd0c4d6d10eae
aleoterob <aleoterob@gmail.com>
2026-05-14 08:48:14 -0300

    feat: initialize transfer-producer (SpringBoot project)

---
+++ transfer-producer/src/main/java/com/aleoterob/transfer_producer/TransferProducerApplication.java
@@ -0,0 +1,13 @@
+package com.aleoterob.transfer_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferProducerApplication.class, args);
+	}
+
+}
