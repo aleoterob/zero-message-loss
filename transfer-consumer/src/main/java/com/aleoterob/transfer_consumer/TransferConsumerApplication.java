f110856c07d83b274824a649acef326453e17013
aleoterob <aleoterob@gmail.com>
2026-05-14 09:49:21 -0300

    feat: initialize transfer-consumer (SpringBoot project)

---
+++ transfer-consumer/src/main/java/com/aleoterob/transfer_consumer/TransferConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.aleoterob.transfer_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferConsumerApplication.class, args);
+	}
+
+}
