c40a18f677e629aa82b9780631ac21c817d180ab
aleoterob <aleoterob@gmail.com>
2026-05-14 17:54:38 -0300

    fix: allow frontend transfer requests

--- transfer-producer/src/main/java/com/aleoterob/transfer_producer/api/TransferController.java
+++ transfer-producer/src/main/java/com/aleoterob/transfer_producer/api/TransferController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/transfers")
 public class TransferController {
 	private final TransferService transferService;
