eb6b9445c4df7c8421e35a80954fe48f702c45e3
aleoterob <aleoterob@gmail.com>
2026-05-16 12:25:03 -0300

    chore: update processed transfers connector

--- debezium/register-consumer-connector.sh
+++ debezium/register-consumer-connector.sh
@@ -5,7 +5,9 @@ until curl -s http://localhost:8083/connectors > /dev/null; do
   sleep 2
 done
 
-curl -X PUT http://localhost:8083/connectors/processed-events-connector/config -H "Content-Type: application/json" -d '{
+curl -s -X DELETE http://localhost:8083/connectors/processed-events-connector > /dev/null || true
+
+curl -X PUT http://localhost:8083/connectors/processed-transfers-connector/config -H "Content-Type: application/json" -d '{
     "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
     "database.hostname": "ep-royal-thunder-actfaxh5.sa-east-1.aws.neon.tech",
     "database.port": "5432",
@@ -13,15 +15,16 @@ curl -X PUT http://localhost:8083/connectors/processed-events-connector/config -
     "database.password": "npg_coPVdtEN6X0p",
     "database.dbname": "neondb",
     "plugin.name": "pgoutput",
-    "slot.name": "processed_events_slot",
-    "publication.name": "processed_events_publication",
-    "table.include.list": "public.processed_events",
+    "slot.name": "processed_transfers_slot",
+    "publication.name": "processed_transfers_publication",
+    "table.include.list": "public.processed_transfers",
+    "decimal.handling.mode": "string",
     "topic.prefix": "transfer-consumer-db",
     "tombstones.on.delete": "false",
     "transforms": "route",
     "transforms.route.type": "org.apache.kafka.connect.transforms.RegexRouter",
-    "transforms.route.regex": "transfer-consumer-db.public.processed_events",
-    "transforms.route.replacement": "consumer.processed-events",
+    "transforms.route.regex": "transfer-consumer-db.public.processed_transfers",
+    "transforms.route.replacement": "consumer.processed-transfers",
     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
     "value.converter.schemas.enable": "false",
     "key.converter": "org.apache.kafka.connect.storage.StringConverter"
